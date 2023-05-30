3dfcb52f97c70cf702b68b5e2f9550cd3aacf588
Chanhyun Wang <rubberramen@naver.com>
2023-05-31 07:28:59 +0900

    day25 setting

---
+++ src/main/java/programmers/lev0/day25_230531/Prac02.java
@@ -0,0 +1,19 @@
+package programmers.lev0.day25_230531;
+
+import java.util.Arrays;
+
+public class Prac02 {
+    public static void main(String[] args) {
+        Prac02 prac01 = new Prac02();
+        int[] answer = prac01.solution(new String[]{"left", "right", "up", "right", "right"}, new int[]{11, 11});
+        System.out.println("answer = " + Arrays.toString(answer));
+    }
+
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+
+}
