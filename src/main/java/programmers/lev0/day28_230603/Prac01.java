f0ece53c031b3a2419eedea933f9be073a3d9915
Chanhyun Wang <rubberramen@naver.com>
2023-06-02 17:17:46 +0900

    day28 setting

---
+++ src/main/java/programmers/lev0/day28_230603/Prac01.java
@@ -0,0 +1,16 @@
+package programmers.lev0.day28_230603;
+
+/* 유한소수 판별하기 : https://school.programmers.co.kr/learn/courses/30/lessons/120878 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        int answer = prac01.solution(7, 20);
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int a, int b) {
+        int answer = 0;
+        return answer;
+    }
+
+}
