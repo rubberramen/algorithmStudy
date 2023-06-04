70bbf3dbcc07f90cb243bb5e77940b65895b22a9
Chanhyun Wang <rubberramen@naver.com>
2023-06-05 08:59:23 +0900

    Solve day30_230605 Prac03

--- src/main/java/programmers/lev0/day30_230605/Prac03.java
+++ src/main/java/programmers/lev0/day30_230605/Prac03.java
@@ -4,12 +4,19 @@ package programmers.lev0.day30_230605;
 public class Prac03 {
     public static void main(String[] args) {
         Prac03 prac01 = new Prac03();
-        int answer = prac01.solution(new int[]{1, 2, 3, 4});
+        int answer = prac01.solution(new int[]{2, 4, 8});  // 5   // [2, 4, 8] -> 16
         System.out.println("answer = " + answer);
     }
 
     public int solution(int[] common) {
         int answer = 0;
+
+        if (common[2] - common[1] == common[1] - common[0]) {   // 등차 수열
+            answer = common[common.length - 1] + common[1] - common[0];
+        } else {    // 등비 수열.
+            answer = common[common.length - 1] * (common[1] / common[0]);
+        }
+
         return answer;
     }
 
