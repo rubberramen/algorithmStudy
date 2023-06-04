c635690eb16465bf4e0d792ed6cd79853ba92662
Chanhyun Wang <rubberramen@naver.com>
2023-06-05 07:22:31 +0900

    Setting again day30, day31

---
+++ src/main/java/programmers/lev0/day30_230605/Prac03.java
@@ -0,0 +1,16 @@
+package programmers.lev0.day30_230605;
+
+/* 다음에 올 숫자 : https://school.programmers.co.kr/learn/courses/30/lessons/120924 */
+public class Prac03 {
+    public static void main(String[] args) {
+        Prac03 prac01 = new Prac03();
+        int answer = prac01.solution(new int[]{1, 2, 3, 4});
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int[] common) {
+        int answer = 0;
+        return answer;
+    }
+
+}
