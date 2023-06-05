fb329352d7c2294fd320ff9d3e7e1e761d100939
Chanhyun Wang <rubberramen@naver.com>
2023-06-05 16:17:52 +0900

    Setting Tomorrow

---
+++ src/main/java/programmers/lev0/day31_230606/Prac03.java
@@ -0,0 +1,16 @@
+package programmers.lev0.day31_230606;
+
+/* 모스부호(1) : https://school.programmers.co.kr/learn/courses/30/lessons/120838 */
+public class Prac03 {
+    public static void main(String[] args) {
+        Prac03 prac01 = new Prac03();
+        String answer = prac01.solution(".... . .-.. .-.. ---");
+        System.out.println("answer = " + answer);
+    }
+
+    public String solution(String letter) {
+        String answer = "";
+        return answer;
+    }
+
+}
