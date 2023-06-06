a5f8135992b9bab440d00d244c8ebb45b7f03ec8
Chanhyun Wang <rubberramen@naver.com>
2023-06-06 20:34:53 +0900

    BaekJoon Setting

---
+++ src/main/java/baekJoon/Test2.java
@@ -0,0 +1,20 @@
+package baekJoon;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//        String next = sc.nextLine();
+//        String[] s = next.split(" ");
+//        int a = Integer.parseInt(s[0]);
+//        int b = Integer.parseInt(s[1]);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
