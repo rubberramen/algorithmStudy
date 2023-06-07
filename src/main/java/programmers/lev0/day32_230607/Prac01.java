6dd09df48abba906607ab4b96b9b3a6e8d584918
Chanhyun Wang <rubberramen@naver.com>
2023-06-07 18:09:03 +0900

    Solve day32_230607

--- src/main/java/programmers/lev0/day32_230607/Prac01.java
+++ src/main/java/programmers/lev0/day32_230607/Prac01.java
@@ -1,16 +1,54 @@
 package programmers.lev0.day32_230607;
 
-/* 모스부호(1) : https://school.programmers.co.kr/learn/courses/30/lessons/120838 */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 옹알이(1) : https://school.programmers.co.kr/learn/courses/30/lessons/120956 */
 public class Prac01 {
     public static void main(String[] args) {
         Prac01 prac01 = new Prac01();
-        String answer = prac01.solution(".... . .-.. .-.. ---");
+        int answer = prac01.solution(new String[]{"ayaye", "uuuma", "ye", "yemawoo", "ayaa"});
         System.out.println("answer = " + answer);
     }
 
-    public String solution(String letter) {
-        String answer = "";
+    public int solution(String[] babbling) {
+        int answer = 0;
+
+        for(int i = 0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replaceFirst("aya", "0");
+            babbling[i] = babbling[i].replaceFirst("woo", "0");
+            babbling[i] = babbling[i].replaceFirst("ye", "0");
+            babbling[i] = babbling[i].replaceFirst("ma", "0");
+            babbling[i] = babbling[i].replace("0", "");
+
+            if(babbling[i].equals("")) answer++;
+        }
         return answer;
     }
 
+    public int solution_fail(String[] babbling) {
+        int answer = 0;
+        String[] able = {"aya", "ye", "woo", "ma"};
+
+        for (String s : babbling) {
+            List<String> list = new ArrayList<>(Arrays.asList(s.split("")));
+
+            for (String s1 : able) {
+                List<String> list1 = new ArrayList<>(Arrays.asList(s1.split("")));
+
+                for (int i = 0; i < list.size(); i++) {
+                    for (int j = 0; j < list1.size(); j++) {
+                        if (list.get(i).equals(list1.get(j))) {  // why???
+                            list.remove(i);
+                        }
+                    }
+                }
+                if (list.isEmpty()) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
 }
--- src/main/java/programmers/lev0/day32_230607/Prac02.java
+++ src/main/java/programmers/lev0/day32_230607/Prac02.java
@@ -1,21 +1,26 @@
 package programmers.lev0.day32_230607;
 
-/* 옹알이(1) : https://school.programmers.co.kr/learn/courses/30/lessons/120956 */
+/* 모스부호(1) : https://school.programmers.co.kr/learn/courses/30/lessons/120838 */
 public class Prac02 {
     public static void main(String[] args) {
         Prac02 prac01 = new Prac02();
-        int answer = prac01.solution(new String[]{"aya", "yee", "u", "maa", "wyeoo"});
+        String answer = prac01.solution(".... . .-.. .-.. ---");
         System.out.println("answer = " + answer);
     }
 
-    public int solution(String[] babbling) {
-        int answer = 0;
-        String[] able = {"aya", "ye", "woo", "ma"};
+    public String solution(String letter) {
+        String answer = "";
 
-        for (String s : babbling) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.",
+                "--.","....","..",".---","-.-",".-..","--","-.",
+                "---",".--.","--.-",".-.","...","-","..-","...-",
+                ".--","-..-","-.--","--.."};
 
-
-
+        String[] split = letter.split(" ");
+        for (String s : split) {
+            for (int i = 0; i < morse.length; i++) {
+                if (s.equals(morse[i])) answer += Character.toString(i + 'a');
+            }
         }
         return answer;
     }
