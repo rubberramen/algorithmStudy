d3374e4601cbfaa8d6d64e9b0ac18d71f8fd30aa
Chanhyun Wang <rubberramen@naver.com>
2023-06-08 08:58:25 +0900

    Setting Programmers day33_230608

---
+++ src/main/java/programmers/lev1/day33_230608/Prac01.java
@@ -0,0 +1,23 @@
+package programmers.lev1.day33_230608;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 자릿수 더하기 : https://school.programmers.co.kr/learn/courses/30/lessons/12931 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        int answer = prac01.solution(123);
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
---
+++ src/main/java/programmers/lev1/day33_230608/Prac02.java
@@ -0,0 +1,15 @@
+package programmers.lev1.day33_230608;
+
+/* 약수의 합 : https://school.programmers.co.kr/learn/courses/30/lessons/12928 */
+public class Prac02 {
+    public static void main(String[] args) {
+        Prac02 prac01 = new Prac02();
+        int answer = prac01.solution(123);
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
