f7931a5b2393b3679e7d5e5f82d804ef4d56423f
Chanhyun Wang <rubberramen@naver.com>
2023-06-09 09:02:49 +0900

    Sort Algorithm Study

--- src/main/java/inflearn/sortingAndSearching/SortTest.java
+++ src/main/java/inflearn/sortingAndSearching/SortTest.java
@@ -10,28 +10,52 @@ public class SortTest {
         int n = kb.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
-        for (int x : T.selection(n, arr)) System.out.print(x + " ");
+        for (int x : T.insertion(n, arr)) System.out.print(x + " ");
     }
 
+    // 선택 정렬.
     private int[] selection(int n, int[] arr) {
 
-        int[] answer = {};
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[minIndex] > arr[j]) minIndex = j;
+            }
 
-        return answer;
+            int tmp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = tmp;
+        }
+
+        return arr;
     }
 
+    // 버블 정렬
     private int[] bubble(int n, int[] arr) {
-
-        int[] answer = {};
-
-        return answer;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
     }
 
+    private int[] bubbleReverse(int n, int[] arr) {
+
+
+        return arr;
+    }
+
+    // 삽입 정렬.
     private int[] insertion(int n, int[] arr) {
         for (int i = 1; i < n; i++) {
             int tmp = arr[i];
             int j;
-            for (j = i - 1; j <= 0; j--) {
+            for (j = i - 1; j >= 0; j--) {
                 if (arr[j] > tmp) {
                     arr[j + 1] = arr[j];
                 } else {
