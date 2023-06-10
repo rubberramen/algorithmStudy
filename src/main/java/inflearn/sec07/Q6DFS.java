d8aba065320cdb314037522cb724c04611588da9
Chanhyun Wang <rubberramen@naver.com>
2023-06-10 22:45:50 +0900

    Inflearn sec7-05

---
+++ src/main/java/inflearn/sec07/Q6DFS.java
@@ -0,0 +1,29 @@
+package inflearn.sec07;
+
+public class Q6DFS {
+    static int n;
+    static int[] ch;
+
+    public void DFS(int L) {
+
+        if (L == n + 1) {
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) tmp += (i + " ");
+            }
+            if (tmp.length() > 0) System.out.println(tmp);
+        } else {
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Q6DFS T = new Q6DFS();
+        n = 3;
+        ch = new int[n + 1];
+        T.DFS(1);
+    }
+}
