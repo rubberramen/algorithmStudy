a809dca2a58b8b88305a4832b9fb0d47b031037c
Chanhyun Wang <rubberramen@naver.com>
2023-06-12 16:45:51 +0900

    Setting Programmers Tomorrow

---
+++ src/main/java/programmers/lev1/day38_230613/Prac01.java
@@ -0,0 +1,19 @@
+package programmers.lev1.day38_230613;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/* 정수 내림차순으로 배치하기 : https://school.programmers.co.kr/learn/courses/30/lessons/12933 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        String answer = prac01.solution(new String[]{"Jane", "Kim"});   // "김서방은 1에 있다"
+        System.out.println("answer = " + answer);
+    }
+
+    public String solution(String[] seoul) {
+        String answer = "";
+        return answer;
+    }
+}
