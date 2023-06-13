de4f0ab823b46d733ad77d333a99ff46239bbee0
Chanhyun Wang <rubberramen@naver.com>
2023-06-13 11:12:15 +0900

    Programmers day39 + Prac04 & Setting Tomorrow

--- src/main/java/programmers/lev1/day38_230613/Prac04.java
+++ src/main/java/programmers/lev1/day38_230613/Prac04.java
@@ -1,22 +1,24 @@
 package programmers.lev1.day38_230613;
 
 
-/* 핸드폰 번호 가리기 : https://school.programmers.co.kr/learn/courses/30/lessons/12948 */
+/* 음양 더하기 : https://school.programmers.co.kr/learn/courses/30/lessons/76501 */
 public class Prac04 {
     public static void main(String[] args) {
         Prac04 prac01 = new Prac04();
-        String answer = prac01.solution("027778888");   // "김서방은 1에 있다"
+        int answer = prac01.solution(new int[]{1, 2, 3}, new boolean[]{false,false,true});   // "김서방은 1에 있다"
         System.out.println("answer = " + answer);
     }
 
-    public String solution(String phone_number) {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
 
-        StringBuilder sb = new StringBuilder(phone_number);
-
-        for (int i = sb.length() - 5; i >= 0; i--) {
-            sb.replace(i, i + 1, "*");
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
         }
-
-        return sb.toString();
+        return answer;
     }
 }
---
+++ src/main/java/programmers/lev1/day39_230614/Prac01.java
@@ -0,0 +1,18 @@
+package programmers.lev1.day39_230614;
+
+
+import java.util.Arrays;
+
+/* 없는 숫자 더하기 : https://school.programmers.co.kr/learn/courses/30/lessons/86051 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        int answer = prac01.solution(new int[]{1,2,3,4,6,7,8,0});   // 14
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int[] numbers) {
+        int answer = -1;
+        return answer;
+    }
+}
