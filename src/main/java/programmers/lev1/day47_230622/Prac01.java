7ac0df7bc604e298ac55d1e5a7e4df2d3c589eec
Chanhyun Wang <rubberramen@naver.com>
2023-06-22 16:27:51 +0900

    Solve Programmers day47_230622 setting tomorrow

--- src/main/java/programmers/lev1/day47_230622/Prac01.java
+++ src/main/java/programmers/lev1/day47_230622/Prac01.java
@@ -1,22 +1,51 @@
 package programmers.lev1.day47_230622;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /* 가장 가까운 같은 글자 : https://school.programmers.co.kr/learn/courses/30/lessons/142086 */
 public class Prac01 {
     public static void main(String[] args) {
         Prac01 prac01 = new Prac01();
-        int[] answer = prac01.solution("banana");   // 3
+        int[] answer = prac01.solution("foobar");   // 3
         System.out.println("answer = " + Arrays.toString(answer));
     }
 
     public int[] solution(String s) {
-        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        list.add(-1);
 
+        String[] split = s.split("");
+        for (int i = 1; i < split.length; i++) {
+            boolean flag = false;
+            int tmp = 0;
+            for (int j = 0; j < i; j++) {
+                if (split[i].equals(split[j])) {
+                    flag = true;
+                    tmp = j;
+                }
+            }
+
+            if (!flag) {
+                list.add(-1);
+            } else {
+                int i1 = i - tmp;
+                list.add(i1);
+            }
+        }
+
+        int[] array = list.stream().mapToInt(Integer::intValue).toArray();
+        return array;
+    }
+
+    public int[] solution_good1(String s) {
+        int[] answer = new int[s.length()];
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            answer[i] = i - map.getOrDefault(ch, i + 1);
+            map.put(ch, i);
+        }
         return answer;
     }
 }
---
+++ src/main/java/programmers/lev1/day48_230623/Prac01.java
@@ -0,0 +1,17 @@
+package programmers.lev1.day48_230623;
+
+
+/* 콜라 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/132267 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        int answer = prac01.solution(2, 1, 20);   // 3
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+
+        return answer;
+    }
+}
