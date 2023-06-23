a12fe7aa1af63edc5b32497aa543063f81793a8f
Chanhyun Wang <rubberramen@naver.com>
2023-06-23 15:03:30 +0900

    Solve Programmers day48_230623 Setting tomorrow

--- src/main/java/programmers/lev1/day48_230623/Prac01.java
+++ src/main/java/programmers/lev1/day48_230623/Prac01.java
@@ -1,17 +1,35 @@
 package programmers.lev1.day48_230623;
 
 
-/* 콜라 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/132267 */
+import java.util.*;
+
+/* 추억 점수 : https://school.programmers.co.kr/learn/courses/30/lessons/176963 */
 public class Prac01 {
     public static void main(String[] args) {
         Prac01 prac01 = new Prac01();
-        int answer = prac01.solution(2, 1, 20);   // 3
-        System.out.println("answer = " + answer);
+        int[] answer = prac01.solution(new String[]{"kali", "mari", "don"},
+                new int[]{11, 1, 55},
+                new String[][]{{"kali", "mari", "don"}, {"pony", "tom", "teddy"}, {"con", "mona", "don"}});   // 3
+        System.out.println("answer = " + Arrays.toString(answer));
     }
 
-    public int solution(int a, int b, int n) {
-        int answer = 0;
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
 
-        return answer;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < photo.length; i++) {
+            int tmp = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                String s = photo[i][j];
+
+                for (int k = 0; k < name.length; k++) {
+                    if (s.equals(name[k])) tmp += yearning[k];
+                }
+            }
+            list.add(tmp);
+        }
+
+        int[] array = list.stream().mapToInt(Integer::intValue).toArray();
+        return array;
     }
 }
---
+++ src/main/java/programmers/lev1/day49_230624/Prac01.java
@@ -0,0 +1,17 @@
+package programmers.lev1.day49_230624;
+
+
+/* 2016년 : https://school.programmers.co.kr/learn/courses/30/lessons/12901 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        String answer = prac01.solution(5, 24);   // 3
+        System.out.println("answer = " + answer);
+    }
+
+    public String solution(int a, int b) {
+        String answer = "";
+
+        return answer;
+    }
+}
