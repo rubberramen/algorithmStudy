c1c6d4ca18cc7d43a1b79e6d8efc344820faf846
Chanhyun Wang <rubberramen@naver.com>
2023-06-24 15:58:14 +0900

    Solve Programmers day49_230624 & Setting tommorow

--- src/main/java/programmers/lev1/day49_230624/Prac01.java
+++ src/main/java/programmers/lev1/day49_230624/Prac01.java
@@ -1,16 +1,29 @@
 package programmers.lev1.day49_230624;
 
 
-/* 폰켓몬: https://school.programmers.co.kr/learn/courses/30/lessons/1845 */
+/* 2016년 : https://school.programmers.co.kr/learn/courses/30/lessons/12901 */
 public class Prac01 {
     public static void main(String[] args) {
         Prac01 prac01 = new Prac01();
-        int answer = prac01.solution(new int[]{3, 1, 2, 3});   // 2
+        String answer = prac01.solution(5, 24);   // "TUE"
         System.out.println("answer = " + answer);
     }
 
-    public int solution(int[] nums) {
-        int answer = 0;
+    // 2016년 1월 1일 : 금요일.
+    public String solution(int a, int b) {
+        String answer = "";
+
+        String[] day = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        int allDate = 0;
+
+        for (int i = 0; i < a - 1; i++) {
+            allDate += date[i];
+        }
+
+        allDate += (b - 1);
+        answer = day[allDate % 7];
 
         return answer;
     }
---
+++ src/main/java/programmers/lev1/day50_230625/Prac01.java
@@ -0,0 +1,17 @@
+package programmers.lev1.day50_230625;
+
+
+/* 폰켓몬: https://school.programmers.co.kr/learn/courses/30/lessons/1845 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        int answer = prac01.solution(new int[]{3, 1, 2, 3});   // 2
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        return answer;
+    }
+}
