e8b8d7eff1ab7fb7f8251700cfc1c057b2216d1e
Chanhyun Wang <rubberramen@naver.com>
2023-06-25 20:22:32 +0900

    Solve Programmers day50_230625 & Setting Tomorrow

--- src/main/java/programmers/lev1/day50_230625/Prac01.java
+++ src/main/java/programmers/lev1/day50_230625/Prac01.java
@@ -1,17 +1,32 @@
 package programmers.lev1.day50_230625;
 
 
-/* 폰켓몬: https://school.programmers.co.kr/learn/courses/30/lessons/1845 */
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/* 두 개 뽑아서 더하기 : https://school.programmers.co.kr/learn/courses/30/lessons/68644 */
 public class Prac01 {
     public static void main(String[] args) {
         Prac01 prac01 = new Prac01();
-        int answer = prac01.solution(new int[]{3, 1, 2, 3});   // 2
-        System.out.println("answer = " + answer);
+        int[] answer = prac01.solution(new int[]{2,1,3,4,1});   // [2,3,4,5,6,7]
+        System.out.println("answer = " + Arrays.toString(answer));
     }
 
-    public int solution(int[] nums) {
-        int answer = 0;
+    public int[] solution(int[] numbers) {
 
-        return answer;
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (i != j) {
+                    set.add(numbers[i] + numbers[j]);
+                }
+            }
+        }
+
+        int[] array = set.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(array);
+        return array;
     }
 }
