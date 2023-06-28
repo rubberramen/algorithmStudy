faa2520eb0daeb9895872363aa74bc2e121db3c3
Chanhyun Wang <rubberramen@naver.com>
2023-06-28 16:13:16 +0900

    Programmers day53 ongoing

--- src/main/java/programmers/lev1/day53_230628/Prac01.java
+++ src/main/java/programmers/lev1/day53_230628/Prac01.java
@@ -18,6 +18,55 @@ public class Prac01 {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "";
 
+        List<String> cardList1 = Arrays.asList(cards1);
+        List<String> cardList2 = Arrays.asList(cards2);
+
+        boolean flag = false;
+
+        for (int i = 0; i < goal.length; i++) {
+            if (goal[i].equals(cardList1.get(0))) {
+
+            } else {
+
+            }
+        }
+
+
         return answer;
+    }
+
+    public String solution_fail(String[] cards1, String[] cards2, String[] goal) {
+//        String answer = "";
+
+        boolean flag1 = false;
+        boolean flag2 = false;
+
+        for (int i = 0; i < goal.length; i++) {
+
+            if (i % 2 == 0) {
+                for (int j = 0; j < cards1.length; j++) {
+                    if (cards1[j].equals(goal[i])) {
+                        flag1 = true;
+                        break;
+                    }
+                }
+                if (!flag1) return "No";
+                flag1 = false;
+            } else {
+                for (int j = 0; j < cards2.length; j++) {
+                    if (cards2[j].equals(goal[i])) {
+                        flag2 = true;
+                        break;
+                    }
+                }
+                if (!flag2) return "No";
+                flag2 = false;
+            }
+        }
+
+//        if (flag1 && flag2) return "Yes";
+//        else return "No";
+        return "Yes";
+
     }
 }
