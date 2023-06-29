2df598c1665ff530bf8b8309efa357f49622948e
Chanhyun Wang <rubberramen@naver.com>
2023-06-30 06:27:32 +0900

    Solve Programmers day53_230628

--- src/main/java/programmers/lev1/day53_230628/Prac01.java
+++ src/main/java/programmers/lev1/day53_230628/Prac01.java
@@ -16,23 +16,23 @@ public class Prac01 {
     }
 
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
-
-        List<String> cardList1 = Arrays.asList(cards1);
-        List<String> cardList2 = Arrays.asList(cards2);
-
-        boolean flag = false;
+        int cardIdx1 = 0;
+        int cardIdx1Max = cards1.length;
+        int cardIdx2 = 0;
+        int cardIdx2Max = cards2.length;
 
         for (int i = 0; i < goal.length; i++) {
-            if (goal[i].equals(cardList1.get(0))) {
-
+            String curStr = goal[i];
+            if (cardIdx1 < cardIdx1Max && curStr.equals(cards1[cardIdx1])) {
+                cardIdx1++;
+            } else if (cardIdx2 < cardIdx2Max && curStr.equals(cards2[cardIdx2])) {
+                cardIdx2++;
             } else {
-
+                return "No";
             }
         }
 
-
-        return answer;
+        return "Yes";
     }
 
     public String solution_fail(String[] cards1, String[] cards2, String[] goal) {
