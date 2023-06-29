728e85390a3bbda08f759be76650d32e885d0a4e
Chanhyun Wang <rubberramen@naver.com>
2023-06-30 06:52:04 +0900

    Setting Programmers day55_230630

---
+++ src/main/java/programmers/lev1/day55_230630/Prac01.java
@@ -0,0 +1,18 @@
+package programmers.lev1.day55_230630;
+
+
+import java.util.*;
+
+/* 덧칠하기: https://school.programmers.co.kr/learn/courses/30/lessons/161989 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        int answer = prac01.solution(8, 4, new int[]{2, 3, 6});   // "[3,4,2,1,5]"
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        return answer;
+    }
+}
