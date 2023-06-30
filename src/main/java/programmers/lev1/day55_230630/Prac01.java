07fc13f6ff3e423eba80caad299541cb2eb2846d
Chanhyun Wang <rubberramen@naver.com>
2023-06-30 13:32:12 +0900

    Solve Programmers day55_230630 $ Setting Tomorrow

--- src/main/java/programmers/lev1/day55_230630/Prac01.java
+++ src/main/java/programmers/lev1/day55_230630/Prac01.java
@@ -12,7 +12,20 @@ public class Prac01 {
     }
 
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        return answer;
+        int start = section[0];
+        int end = section[0] + (m-1);
+        int ans = 1;
+
+        for(int i : section){
+             if(i>=start && i<=end)
+                continue;
+            else{
+                start = i;
+                end = i + (m-1);
+                ans++;
+            }
+
+        }
+        return ans;
     }
 }
---
+++ src/main/java/programmers/lev1/day56_230701/Prac01.java
@@ -0,0 +1,17 @@
+package programmers.lev1.day56_230701;
+
+
+/* [1차] 다트 게임: https://school.programmers.co.kr/learn/courses/30/lessons/17682 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        int answer = prac01.solution("1S2D*3T");   // 37
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(String dartResult) {
+        int answer = 0;
+        
+        return answer;
+    }
+}
