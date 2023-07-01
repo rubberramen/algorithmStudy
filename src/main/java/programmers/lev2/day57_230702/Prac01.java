81b2a55f3a66c12898ed6d9858e2eb63085375e4
Chanhyun Wang <rubberramen@naver.com>
2023-07-01 09:53:47 +0900

    Solve Programmers day56_230701 & Setting Tomorrow

---
+++ src/main/java/programmers/lev2/day57_230702/Prac01.java
@@ -0,0 +1,16 @@
+package programmers.lev2.day57_230702;
+
+
+/* JadenCase 문자열 만들기: https://school.programmers.co.kr/learn/courses/30/lessons/12951 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        String answer = prac01.solution("1S2D*3T");   // 37
+        System.out.println("answer = " + answer);
+    }
+
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+}
