887b073f172540a7626e269f1ece23d1c38a41cb
Chanhyun Wang <rubberramen@naver.com>
2023-07-07 17:00:07 +0900

    Solve Programmers day62_230707 & Setting Tomorrow

--- src/main/java/programmers/lev2/day62_230707/Prac01.java
+++ src/main/java/programmers/lev2/day62_230707/Prac01.java
@@ -1,20 +1,56 @@
 package programmers.lev2.day62_230707;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Stack;
+import java.util.List;
 
 /* 카펫: https://school.programmers.co.kr/learn/courses/30/lessons/42842 */
 public class Prac01 {
     public static void main(String[] args) {
         Prac01 prac01 = new Prac01();
-        int[] answer = prac01.solution(10, 2);   // 1
+        int[] answer = prac01.solution(24, 24);   // 1
         System.out.println("answer = " + Arrays.toString(answer));
+
+//        prac01.test(24);
     }
 
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
+        int[] answer = new int[2];
 
-        return answer;
+        if (yellow == 1) {
+            int i = brown + yellow;
+            int sqrt = (int) Math.sqrt(i);
+            answer[0] = sqrt;
+            answer[1] = sqrt;
+            return answer;
+        } else {
+            List<Integer> test = test(yellow);
+            for (Integer a : test) {
+                int b = yellow / a;
+
+                int i = 2 * a + 2 * b + 4;
+
+                if (i == brown) {
+                    answer[0] = b + 2;
+                    answer[1] = a + 2;
+                }
+
+            }
+
+            return answer;
+        }
+
+    }
+
+    public List<Integer> test(int n) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = n / 2; i >= 1; i--) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+        return list;
     }
 }
---
+++ src/main/java/programmers/lev2/day63_230708/Prac01.java
@@ -0,0 +1,17 @@
+package programmers.lev2.day63_230708;
+
+
+/* 예상 대진표: https://school.programmers.co.kr/learn/courses/30/lessons/12985 */
+public class Prac01 {
+    public static void main(String[] args) {
+        Prac01 prac01 = new Prac01();
+        int answer = prac01.solution(8, 4, 7);   // 3
+        System.out.println("answer = " + answer);
+    }
+
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        return answer;
+    }
+}
