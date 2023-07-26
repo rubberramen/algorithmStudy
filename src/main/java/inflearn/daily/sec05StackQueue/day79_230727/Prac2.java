58b21df3a2085209666356fd443da4c58707994b
Chanhyun Wang <rubberramen@naver.com>
2023-07-26 10:45:26 +0900

    Solve Inflearn algorithm Lecture day78_230726 & Setting Tomorrow

---
+++ src/main/java/inflearn/daily/sec05StackQueue/day79_230727/Prac2.java
@@ -0,0 +1,33 @@
+package inflearn.daily.sec05StackQueue.day79_230727;
+
+/* 2. 괄호문자제거 : https://cote.inflearn.com/contest/10/problem/05-02 */
+
+import java.util.*;
+
+public class Prac2 {
+    public static void main(String[] args) {
+        Prac2 T = new Prac2();
+//        Scanner kb = new Scanner(System.in);
+//        String str = kb.next();
+
+        String str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+        System.out.println(T.solution(str));
+    }
+
+    public String solution(String str) {
+        String answer = "";
+
+        return answer;
+    }
+    public String solution_answer(String str) {
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == ')') {
+                while (stack.pop() != '(') ;
+            } else stack.push(x);
+        }
+        for (int i = 0; i < stack.size(); i++) answer += stack.get(i);
+        return answer;
+    }
+}
--- src/main/java/inflearn/daily/sec4HashMapTreeSet/day78_230726/Prac5.java
+++ src/main/java/inflearn/daily/sec4HashMapTreeSet/day78_230726/Prac5.java
@@ -2,8 +2,7 @@ package inflearn.daily.sec4HashMapTreeSet.day78_230726;
 
 /* 5. K번째 큰 수 : https://cote.inflearn.com/contest/10/problem/04-05 */
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Prac5 {
     public static void main(String[] args) {
@@ -15,13 +14,56 @@ public class Prac5 {
         for (int i = 0; i < n; i++) {
             arr[i] = kb.nextInt();
         }
+
+//        int n = 10;
+//        int k = 3;
+//        int[] arr = {13, 15, 34, 23, 45, 65, 33, 11, 26, 42};
+
         System.out.println(T.solution(arr, n, k));
     }
 
     public int solution(int[] arr, int n, int k) {
         int answer = -1;
 
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    Tset.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+        int cnt = 0;
+        //Tset.remove(143);
+        //System.out.println(Tset.size());
+        //System.out.println("first : "+ Tset.first());
+        //System.out.println("last : "+ Tset.last());
+
+        for (int x : Tset) {
+            //System.out.println(x);
+            cnt++;
+            if (cnt == k) return x;
+        }
         return answer;
     }
 
+    public int solution_me(int[] arr, int n, int k) {
+        int answer = -1;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int l = j + 1; l < arr.length; l++) {
+                    set.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+        list.sort(Collections.reverseOrder());
+
+        return list.size() < k ? answer : list.get(k - 1);
+    }
 }
