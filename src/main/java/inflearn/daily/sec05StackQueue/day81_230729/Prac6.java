d03e03164c60ad128fce0fffc6e2d9299718ec05
Chanhyun Wang <rubberramen@naver.com>
2023-07-29 10:04:32 +0900

    Solve Inflearn algorithm Lecture day81_230729 & Setting Tomorrow

--- src/main/java/inflearn/daily/sec05StackQueue/day81_230729/Prac6.java
+++ src/main/java/inflearn/daily/sec05StackQueue/day81_230729/Prac6.java
@@ -10,17 +10,53 @@ import java.util.Stack;
 public class Prac6 {
     public static void main(String[] args) {
         Prac6 T = new Prac6();
-//        Scanner kb = new Scanner(System.in);
-//        int n = kb.nextInt();
-//        int k = kb.nextInt();
-        int n = 8;
-        int k = 3;
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+//        int n = 8;
+//        int k = 3;
         System.out.println(T.solution(n, k));
     }
 
     public int solution(int n, int k) {
         int answer = 0;
         Queue<Integer> Q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) Q.offer(i);
+
+        while (!Q.isEmpty()) {
+            for (int i = 1; i < k; i++) {
+                Q.offer(Q.poll());
+            }
+            Q.poll();
+
+            if (Q.size() == 1) {
+                answer = Q.poll();
+            }
+        }
+        return answer;
+    }
+
+    public int solution_me(int n, int k) {
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        while (!q.isEmpty()) {
+
+            for (int i = 1; i < k; i++) {
+                q.offer(q.poll());
+            }
+
+            q.poll();
+
+            if (q.size() == 1) {
+                answer = q.poll();
+            }
+        }
 
         return answer;
     }
---
+++ src/main/java/inflearn/daily/sec05StackQueue/day82_230730/Prac7.java
@@ -0,0 +1,27 @@
+package inflearn.daily.sec05StackQueue.day82_230730;
+
+/* 7. 교육과정 설계 : https://cote.inflearn.com/contest/10/problem/05-07 */
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Prac7 {
+    public static void main(String[] args) {
+        Prac7 T = new Prac7();
+//        Scanner kb = new Scanner(System.in);
+//        String a = kb.next();
+//        String b = kb.next();
+
+        String a = "CBA";
+        String b = "CBDAGE";
+        System.out.println(T.solution(a, b));
+    }
+
+    public String solution(String need, String plan) {
+        String answer = "YES";
+        Queue<Character> Q = new LinkedList<>();
+
+        return answer;
+    }
+}
