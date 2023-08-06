6988b1df36f4aa7eda438c2b9a8c96d5a83d2fba
Chanhyun Wang <rubberramen@naver.com>
2023-08-06 22:33:24 +0900

    Solve Inflearn algorithm Lecture day89_230806 & Setting Tomorrow

--- src/main/java/inflearn/daily/sec06SortingSearching/day89_230806/Prac8.java
+++ src/main/java/inflearn/daily/sec06SortingSearching/day89_230806/Prac8.java
@@ -10,19 +10,44 @@ public class Prac8 {
     public static void main(String[] args) {
         Prac8 T = new Prac8();
         Scanner kb = new Scanner(System.in);
-//        int n=kb.nextInt();
-//        int m=kb.nextInt();
-//        int[] arr=new int[n];
-//        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
 
-        int n = 8;
-        int m = 32;
-        int[] arr = {23, 87, 65, 12, 57, 32, 99, 81};
+//        int n = 8;
+//        int m = 32;
+//        int[] arr = {23, 87, 65, 12, 57, 32, 99, 81};
         System.out.println(T.solution(n, m, arr));
     }
 
-    public int solution(int n, int m, int[] arr){
-        int answer=0;
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0;
+        int rt = n - 1;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+            if (arr[mid] > m) rt = mid - 1;
+            else lt = mid + 1;
+        }
+        return answer;
+    }
+
+    public int solution_me(int n, int m, int[] arr) {
+        int answer = 0;
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == m) {
+                answer = i + 1;
+            }
+        }
 
         return answer;
     }
---
+++ src/main/java/inflearn/daily/sec06SortingSearching/day90_230807/Prac9.java
@@ -0,0 +1,28 @@
+package inflearn.daily.sec06SortingSearching.day90_230807;
+
+/* 9. 뮤직비디오(결정알고리즘) : https://cote.inflearn.com/contest/10/problem/06-09 */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Prac9 {
+    public static void main(String[] args) {
+        Prac9 T = new Prac9();
+        Scanner kb = new Scanner(System.in);
+//        int n = kb.nextInt();
+//        int m = kb.nextInt();
+//        int[] arr = new int[n];
+//        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+
+        int n = 9;
+        int m = 3;
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(T.solution(n, m, arr));
+    }
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+
+        return answer;
+    }
+}
