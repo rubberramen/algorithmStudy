aea39b871914174bac0bff4a17ef14b96ca4f6ff
Chanhyun Wang <rubberramen@naver.com>
2023-08-07 06:59:46 +0900

    [Ongoing] Inflearn algorithm Lecture day90_230807

--- src/main/java/inflearn/daily/sec06SortingSearching/day90_230807/Prac9.java
+++ src/main/java/inflearn/daily/sec06SortingSearching/day90_230807/Prac9.java
@@ -22,7 +22,31 @@ public class Prac9 {
 
     public int solution(int n, int m, int[] arr) {
         int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();  // 9
+        int rt = Arrays.stream(arr).sum();             // 45
 
+//        int lt = Arrays.stream(arr).sum() / n;
+//        int rt = Arrays.stream(arr).sum() / n - 1;
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else lt = mid + 1;
+        }
         return answer;
+    }
+
+    public int count(int[] arr, int capacity) {
+        int cnt = 1;
+        int sum = 0;
+        for (int x : arr) {
+            if (sum + x > capacity) {
+                cnt++;
+                sum = x;
+            } else sum += x;
+        }
+        return cnt;
     }
 }
