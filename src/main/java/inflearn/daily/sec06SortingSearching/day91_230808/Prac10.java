216c8bf83b38aebe4c2ebf69de2e27e5c0e7322d
Chanhyun Wang <rubberramen@naver.com>
2023-08-08 13:48:55 +0900

    Solve Inflearn algorithm Lecture day91_230808 & Setting Tomorrow

--- src/main/java/inflearn/daily/sec06SortingSearching/day91_230808/Prac10.java
+++ src/main/java/inflearn/daily/sec06SortingSearching/day91_230808/Prac10.java
@@ -9,26 +9,43 @@ public class Prac10 {
     public static void main(String[] args) {
         Prac10 T = new Prac10();
         Scanner kb = new Scanner(System.in);
-//        int n = kb.nextInt();
-//        int c = kb.nextInt();
-//        int[] arr = new int[n];
-
-        int n = 5;
-        int c = 3;
-        int[] arr = {1, 2, 8, 4, 9};
+        int n = kb.nextInt();
+        int c = kb.nextInt();
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+
+//        int n = 5;
+//        int c = 3;
+//        int[] arr = {1, 2, 8, 4, 9};
+
         System.out.println(T.solution(n, c, arr));
     }
 
     public int solution(int n, int c, int[] arr) {
         int answer = 0;
 
+        Arrays.sort(arr);   // 1, 2, 4, 8, 9
+        int lt = 1;
+        int rt = arr[n - 1];   // 9   arr[n - 1] - arr[0]
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;   // 가장 가까운 두 말의 거리를 mid라고 하고 배치할 때 3마리 배치할 수 있는지
+            if (count(arr, mid) >= c) {
+                answer = mid;
+                lt = mid + 1;
+            } else rt = mid - 1;
+        }
         return answer;
     }
 
-    public int count(int[] arr, int dist) {
+    public int count(int[] arr, int dist) {  // 몇마리를 배치할 수 있는지.
         int cnt = 1;
-
+        int ep = arr[0];   // ep : end position.
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
         return cnt;
     }
 }
---
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day92_230809/Prac1.java
@@ -0,0 +1,27 @@
+package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day92_230809;
+
+/* 1. 재귀함수 : 자연수 N이 입력되면 재귀함수를 이용하여 1부터 N까지를 출력하는 프로그램을 작성하세요. */
+
+import java.util.Scanner;
+
+public class Prac1 {
+    public static void main(String[] args) {
+        Prac1 T = new Prac1();
+        T.solution(3);
+        //System.out.println(T.solution(3));
+    }
+
+    public void solution(int n){
+//        DFS(n);
+    }
+
+//    public void DFS(int n){
+//        if(n==0) return;
+//        else{
+//            DFS(n-1);
+//            System.out.print(n+" ");
+//        }
+//    }
+
+
+}
