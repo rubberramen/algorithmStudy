0a1bb595bdddf45d18111675fa3846869fda72c4
Chanhyun Wang <rubberramen@naver.com>
2023-08-12 08:31:57 +0900

    Solve Inflearn algorithm Lecture day95_230812 & Setting Tomorrow

--- src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day95_230812/Prac5.java
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day95_230812/Prac5.java
@@ -1,25 +1,46 @@
 package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day95_230812;
 
 /* 5. 이진트리 순회(깊이우선탐색) :
-*/
+ */
+
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+
+    public Node(int val) {
+        data = val;
+        lt = null;
+        rt = null;
+    }
+}
 
 public class Prac5 {
 
-    static int[] fibo;
+    Node root;
 
     public static void main(String[] args) {
-        Prac5 T = new Prac5();
-        int n = 45;
-        fibo = new int[n + 1];
-        T.DFS(n);  // 배열에 다 기록됨.
-        for (int i = 1; i <= n; i++) System.out.print(fibo[i] + " ");
+        Prac5 tree = new Prac5();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        // 말단 노드의 lr, rt는 null.
+        tree.DFS(tree.root);
     }
 
-    public int DFS(int n) {
-        if (fibo[n] > 0) return fibo[n];  // 여기가 시간 단축의 핵심.
-
-        if (n == 1) return fibo[n] = 1;
-        else if (n == 2) return fibo[n] = 1;
-        else return fibo[n] = DFS(n - 2) + DFS(n - 1);
+    public void DFS(Node root) {
+        if (root == null)  // 말단 노드?
+            return;
+        else {
+//            System.out.print(root.data + " ");  // 전위순회
+            DFS(root.lt);
+            System.out.print(root.data + " ");   // 중위순회
+            DFS(root.rt);
+//            System.out.print(root.data + " ");   // 후위순회
+        }
     }
 }
---
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day96_230813/Prac6.java
@@ -0,0 +1,22 @@
+package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day96_230813;
+
+/* 6. 부분집합 구하기(DFS) :
+자연수 N이 주어지면 1부터 N까지의 원소를 갖는 집합의 부분집합을 모두 출력하는 프로그램을 작성하세요.
+*/
+
+public class Prac6 {
+
+    static int n;
+    static int[] ch;
+
+    public static void main(String[] args) {
+        Prac6 T = new Prac6();
+//        n = 3;
+//        ch = new int[n + 1];
+        T.DFS(1);
+    }
+
+    public void DFS(int L) {
+
+    }
+}
