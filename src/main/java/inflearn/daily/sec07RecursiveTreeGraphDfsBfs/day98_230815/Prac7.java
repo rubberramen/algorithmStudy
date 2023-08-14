24515dcbe402421154ad0f22937c075ccbc8732d
Chanhyun Wang <rubberramen@naver.com>
2023-08-15 05:48:30 +0900

    Solve Inflearn algorithm Lecture day98_230815 & Setting Tomorrow

--- src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day98_230815/Prac7.java
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day98_230815/Prac7.java
@@ -1,14 +1,54 @@
 package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day98_230815;
 
 /* 7. 이진트리 순회(넓이우선탐색 : 레벨 탐색) :
-*   - 이진트리 레벨탐색(BFS : Breadth-First Search)
-* */
+ *   - 이진트리 레벨탐색(BFS : Breadth-First Search)
+ * */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node lt, rt;
+
+    public Node(int val) {
+        data = val;
+        lt = rt = null;
+    }
+}
 
 public class Prac7 {
 
-    public static void main(String[] args) {
-        Prac7 T = new Prac7();
+    Node root;
 
+    public static void main(String[] args) {
+        Prac7 tree = new Prac7();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.BFS(tree.root);
     }
 
+    public void BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.add(root);  // Q.offer(root);
+        int L = 0;
+
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            System.out.print(L + " 레벨 : ");
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                System.out.print(cur.data + " ");
+                if (cur.lt != null) Q.add(cur.lt);
+                if (cur.rt != null) Q.add(cur.rt);
+            }
+            L++;
+            System.out.println();
+        }
+    }
 }
---
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day99_230816/Prac8.java
@@ -0,0 +1,35 @@
+package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day99_230816;
+
+/* 8. 송아지 찾기(BFS : 상태트리탐색) :
+현수는 송아지를 잃어버렸다. 다행히 송아지에는 위치추적기가 달려 있다.
+현수의 위치와 송아지의 위치가 수직선상의 좌표 점으로 주어지면
+현수는 현재 위치에서 송아지의 위치까지 다음과 같은 방법으로 이동한다.
+송아지는 움직이지 않고 제자리에 있다.
+현수는 스카이 콩콩을 타고 가는데 한 번의 점프로 앞으로 1, 뒤로 1, 앞으로 5를 이동할 수 있다.
+최소 몇 번의 점프로 현수가 송아지의 위치까지 갈 수 있는지 구하는 프로그램을 작성하세요.
+*/
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Prac8 {
+
+//    int answer = 0;
+//    int[] dis = {1, -1, 5};
+//    int[] ch;
+//    Queue<Integer> Q = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Prac8 T = new Prac8();
+        Scanner kb = new Scanner(System.in);
+        int s = kb.nextInt();
+        int e = kb.nextInt();
+        System.out.println(T.BFS(s, e));
+    }
+
+    public int BFS(int s, int e) {
+
+        return 0;
+    }
+}
