f1176d46957d62c1e6c8460b2a8335db6d3d5b01
Chanhyun Wang <rubberramen@naver.com>
2023-08-14 10:23:00 +0900

    Solve Inflearn algorithm Lecture day97_230814 & Setting Tomorrow

--- src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day97_230814/Prac6.java
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day97_230814/Prac6.java
@@ -6,17 +6,40 @@ package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day97_230814;
 
 public class Prac6 {
 
+    // 집합 원소의 갯수
     static int n;
+
+    // Check 배열. : 사용한다 or 안한다 표시
     static int[] ch;
 
     public static void main(String[] args) {
         Prac6 T = new Prac6();
-//        n = 3;
-//        ch = new int[n + 1];
+        n = 3;
+        ch = new int[n + 1];  // 인덱스 번호를 원소로 생각하겠다.
         T.DFS(1);
     }
 
     public void DFS(int L) {
+        if (L == n + 1) {
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) {
+                    tmp += i + " ";
+                }
+            }
 
+            if (tmp.length() > 0) {
+                System.out.println(tmp);
+            } else {
+                System.out.print("공집합");
+            }
+
+        } else {
+            ch[L] = 1;  // 사용함
+            DFS(L + 1);  // 왼쪽
+
+            ch[L] = 0;  // 사용하지 않음
+            DFS(L + 1);  // 오른쪽
+        }
     }
 }
---
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day98_230815/Prac7.java
@@ -0,0 +1,14 @@
+package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day98_230815;
+
+/* 7. 이진트리 순회(넓이우선탐색 : 레벨 탐색) :
+*   - 이진트리 레벨탐색(BFS : Breadth-First Search)
+* */
+
+public class Prac7 {
+
+    public static void main(String[] args) {
+        Prac7 T = new Prac7();
+
+    }
+
+}
