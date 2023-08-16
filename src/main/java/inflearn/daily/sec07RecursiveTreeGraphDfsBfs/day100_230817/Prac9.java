bfecdfa75bd5a6e875323227a6f4b937804273dd
Chanhyun Wang <rubberramen@naver.com>
2023-08-16 16:56:33 +0900

    Solve Inflearn algorithm Lecture day99_230816 & Setting Tomorrow

---
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day100_230817/Prac9.java
@@ -0,0 +1,38 @@
+package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day100_230817;
+
+/* 9. Tree 말단 노드까지의 가장 짧은 경로 : DFS
+아래 그림과 같은 이진트리에서 루트 노드 1에서 말단노드까지의 길이 중 가장 짧은 길이를 구하는 프로그램을 작성하세요.
+각 경로의 길이는 루트노드에서 말단노드까지 가는데 이동하는 횟수를 즉 간선(에지)의 개수를 길이로 하겠습니다.
+*/
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node lt, rt;
+
+    public Node(int val) {
+        data = val;
+        lt = rt = null;
+    }
+}
+
+public class Prac9 {
+
+    Node root;
+
+    public static void main(String[] args) {
+        Prac9 tree = new Prac9();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.DFS(0, tree.root));
+    }
+
+    public int DFS(int L, Node root) {
+        return 0;
+    }
+}
--- src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day99_230816/Prac8.java
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day99_230816/Prac8.java
@@ -1,12 +1,12 @@
 package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day99_230816;
 
 /* 8. 송아지 찾기(BFS : 상태트리탐색) :
-현수는 송아지를 잃어버렸다. 다행히 송아지에는 위치추적기가 달려 있다.
-현수의 위치와 송아지의 위치가 수직선상의 좌표 점으로 주어지면
-현수는 현재 위치에서 송아지의 위치까지 다음과 같은 방법으로 이동한다.
-송아지는 움직이지 않고 제자리에 있다.
-현수는 스카이 콩콩을 타고 가는데 한 번의 점프로 앞으로 1, 뒤로 1, 앞으로 5를 이동할 수 있다.
-최소 몇 번의 점프로 현수가 송아지의 위치까지 갈 수 있는지 구하는 프로그램을 작성하세요.
+    현수는 송아지를 잃어버렸다. 다행히 송아지에는 위치추적기가 달려 있다.
+    현수의 위치와 송아지의 위치가 수직선상의 좌표 점으로 주어지면
+    현수는 현재 위치에서 송아지의 위치까지 다음과 같은 방법으로 이동한다.
+    송아지는 움직이지 않고 제자리에 있다.
+    현수는 스카이 콩콩을 타고 가는데 한 번의 점프로 앞으로 1, 뒤로 1, 앞으로 5를 이동할 수 있다.
+    최소 몇 번의 점프로 현수가 송아지의 위치까지 갈 수 있는지 구하는 프로그램을 작성하세요.
 */
 
 import java.util.LinkedList;
@@ -16,20 +16,52 @@ import java.util.Scanner;
 public class Prac8 {
 
 //    int answer = 0;
-//    int[] dis = {1, -1, 5};
-//    int[] ch;
-//    Queue<Integer> Q = new LinkedList<>();
+    int[] dis = {1, -1, 5};
+    int[] ch;   // 체크 배열 : 한번 갔던 곳 체크
+    Queue<Integer> Q = new LinkedList<>();
 
     public static void main(String[] args) {
         Prac8 T = new Prac8();
         Scanner kb = new Scanner(System.in);
         int s = kb.nextInt();
         int e = kb.nextInt();
+//        int s = 8;  // 5
+//        int e = 3;  // 14
         System.out.println(T.BFS(s, e));
     }
 
     public int BFS(int s, int e) {
+        ch = new int[10001];
+        ch[s] = 1;    // 출발 지점.
+        Q.offer(s);   // 방문한 곳 que에 넣기
+        int L = 0;
 
+        while (!Q.isEmpty()) {
+            int len = Q.size();   // 1레벨에서는 3개가 들어감. - 레벨에 있는 원소의 갯수.
+            for (int i = 0; i < len; i++) {
+                // 부모노드
+                int x = Q.poll();
+
+//                if (x == e) {
+//                    return L;
+//                }
+
+                for (int j = 0; j < 3; j++) {   // int[] dis = {1, -1, 5};
+                    // next
+                    int nx = x + dis[j];
+
+                    if (nx == e) {
+                        return L + 1;
+                    }
+
+                    if (nx >= 1 && nx <= 10000 && ch[nx] == 0) {  // 방문 안한 것.
+                        ch[nx] = 1;
+                        Q.offer(nx);  // 방문한 곳 que에 넣기
+                    }
+                }
+            }
+            L++;
+        }
         return 0;
     }
 }
