08523633982b81e587ac8b234497ae33eee04427
Chanhyun Wang <rubberramen@naver.com>
2023-08-17 13:54:30 +0900

    Solve Inflearn algorithm Lecture day100_230817 & Setting Tomorrow

--- src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day101_230818/Prac10.java
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day101_230818/Prac10.java
@@ -1,12 +1,9 @@
-package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day100_230817;
+package inflearn.daily.sec07RecursiveTreeGraphDfsBfs.day101_230818;
 
-/* 9. Tree 말단 노드까지의 가장 짧은 경로 : DFS
+/* 10. Tree 말단 노드까지의 가장 짧은 경로 : BFS
 아래 그림과 같은 이진트리에서 루트 노드 1에서 말단노드까지의 길이 중 가장 짧은 길이를 구하는 프로그램을 작성하세요.
 각 경로의 길이는 루트노드에서 말단노드까지 가는데 이동하는 횟수를 즉 간선(에지)의 개수를 길이로 하겠습니다.
 */
-
-import java.util.LinkedList;
-import java.util.Queue;
 
 class Node {
     int data;
@@ -20,32 +17,22 @@ class Node {
     }
 }
 
-public class Prac9 {
+public class Prac10 {
 
     Node root;
 
     public static void main(String[] args) {
-        Prac9 tree = new Prac9();
+        Prac10 tree = new Prac10();
         tree.root = new Node(1);
         tree.root.lt = new Node(2);
         tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
 
-//        tree.root.rt.lt = new Node(6);
-//        tree.root.rt.rt = new Node(7);
-//
-//        tree.root.lt.lt.lt = new Node(8);
-//        tree.root.lt.lt.rt = new Node(9);
-        System.out.println(tree.DFS(0, tree.root));
-//        System.out.println(tree.DFS(0, tree.root));
+        System.out.println(tree.DFS(tree.root));
     }
 
-    public int DFS(int L, Node root) {
-        if (root.lt == null && root.rt == null) {
-            return L;
-        } else {
-            return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
-        }
+    public int DFS(Node root) {
+        return 0;
     }
 }
