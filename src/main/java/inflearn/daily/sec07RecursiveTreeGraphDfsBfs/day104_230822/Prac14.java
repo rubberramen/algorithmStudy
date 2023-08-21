e4641e9248dd0bcc54905953eca3a2e699b8217a
Chanhyun Wang <rubberramen@naver.com>
2023-08-22 07:15:47 +0900

    Solve Inflearn algorithm Lecture day104_230822 & Setting Tomorrow

--- src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day104_230822/Prac14.java
+++ src/main/java/inflearn/daily/sec07RecursiveTreeGraphDfsBfs/day104_230822/Prac14.java
@@ -21,11 +21,39 @@ public class Prac14 {
         n = kb.nextInt();
         m = kb.nextInt();
 
+        graph = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n + 1];
+        dis = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            graph.get(a).add(b);
+        }
+        T.BFS(1);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i + " : " + dis[i]);
+        }
 
 
     }
 
     public void BFS(int v) {
-
+        ch[v] = 1;
+        dis[v] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+        while (!queue.isEmpty()) {
+            int cv = queue.poll();
+            for (int nv : graph.get(cv)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    queue.offer(nv);
+                    dis[nv] = dis[cv] + 1;
+                }
+            }
+        }
     }
 }
